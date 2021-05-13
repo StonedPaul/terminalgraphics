b59d597b0040fa6ea2a739bc04622028709b2a22
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-05-13 09:34:35 -0400

    Update bounce.py

--- bounce.py
+++ bounce.py
@@ -7,31 +7,39 @@ def switch(boolean):
         return False
     return True
 
-def nextFrame(x,y,up,side,wall,row):
-    amount = random.randint(1, 3)
-    if x < 200 and side:
-        x+= random.randint(amount, 4)
-    elif x > 0 and not(side):
-        x-= random.randint(amount, 4)
-    else:
-        side = switch(side)
-        wall = True
-    if y < 60 and up:
-        y+= random.randint(amount, 4)
-    elif y > 0 and not(up):
-        y-= random.randint(amount, 4)
-    else:
-        up = switch(up)
-        wall = True
+def hitWall(string):
+    if '-' in string:
+        return '000'
+    return '---'
 
-    time.sleep(0.1)
-    os.system("clear")
-    if wall:
-        row = 3*(random.choice('*#%'))
-        wall = False
+def printBlock(x,y,row):
     print(int(y-1)*'\n')
     print(int(x-2)*' '+ row)
     print(int(x-2)*' '+ row)
     print(int(x-2)*' '+ row)
+
+def nextFrame(x,y,up,side,wall,row):
+    amount = random.randint(1, 3)
+    if x < 204 and side:
+        x+= 1
+    elif x > 0 and not(side):
+        x-= 1
+    else:
+        side = switch(side)
+        wall = True
+    if y < 61 and up:
+        y+= 1
+    elif y > 0 and not(up):
+        y-= 1
+    else:
+        up = switch(up)
+        wall = True
+
+    time.sleep(0.01)
+    os.system("clear")
+    if wall:
+        row = hitWall(row)
+        wall = False
+    printBlock(x,y,row)
     nextFrame(x, y, up, side, wall, row)
 nextFrame(1, 1, True, True, True, '***')
